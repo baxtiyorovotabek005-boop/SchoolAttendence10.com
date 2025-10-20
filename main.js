c6881652c8026745bdc6489a50c294659e51bb2c
baxtiyorovotabek005-boop <baxtiyorovotabek005@gmail.com>
2025-10-20 19:33:56 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function updateClock() {
+    let now = new Date();
+    let hours = String(now.getHours()).padStart(2, "0");
+    let minutes = String(now.getMinutes()).padStart(2, "0");
+    let seconds = String(now.getSeconds()).padStart(2, "0");
+
+    document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Har sekundda yangilash
+setInterval(updateClock, 1000);
+updateClock();
